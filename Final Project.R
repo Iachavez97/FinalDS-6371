a518b2135314da7b615da692ca6b10a9561b9088
Iachavez97 <142704040+Iachavez97@users.noreply.github.com>
2023-11-26 23:09:14 -0500

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -32,6 +32,81 @@ plot(hdT2Updated$GrLivArea, hdT2Updated$SalePrice,
      xlab = "living area in sqft", ylab = "Sale Price")
 plot(hd2Updated)
 
+# plot of living area v sale price
+plot(hdT2Updated$GrLivArea, hdT2Updated$SalePrice,
+     xlab = "living area in sqft", ylab = "Sale Price")
+plot(hd2Updated)
+
+# Create a scatterplot of Living area v Sale Price
+hdT2Updated %>% ggplot(aes(x = GrLivArea, y = SalePrice)) +
+  geom_point()  + geom_smooth(method = "lm", color = "red") +
+  labs(x = "Living area in sqft", y = "Sale Price", title = "Scatterplot of Living area v Sale Price")
+
+# multiple linear regression comparing the sq living area to the sale price & neighborhood
+fit <- lm(SalePrice~GrLivArea + Neighborhood, data = hdT2Updated)
+summary(fit)
+par(mfrow=c(2,2))
+plot(fit)
+
+confint(fit)
+#first try the adjusted r squared was pretty low will continue and try to 
+#have a better fitting model
+
+# Create a scatterplot of Living area v Sale Price - taking log of x value
+hdT2Updated %>% ggplot(aes(x = log(GrLivArea), y = SalePrice)) +
+  geom_point()  + geom_smooth(method = "lm", color = "red") +
+  labs(x = "Living area in sqft", y = "Sale Price", title = "Scatterplot of Living area v Sale Price")
+
+# Create a scatterplot of Living area v Sale Price - taking log of y value
+hdT2Updated %>% ggplot(aes(x = GrLivArea, y = log(SalePrice))) +
+  geom_point()  + geom_smooth(method = "lm", color = "red") +
+  labs(x = "Living area in sqft", y = "Sale Price", title = "Scatterplot of Living area v Sale Price")
+
+# Create a scatterplot of Living area v Sale Price - taking log of both value
+hdT2Updated %>% ggplot(aes(x = log(GrLivArea), y = log(SalePrice))) +
+  geom_point()  + geom_smooth(method = "lm", color = "red") +
+  labs(x = "Living area in sqft", y = "Sale Price", title = "Scatterplot of Living area v Sale Price")
+# fitting the model with log of living area
+fit2 <- lm(SalePrice~log(GrLivArea) + Neighborhood, data = hdT2Updated)
+summary(fit2)
+
+fit3 <- lm(log(SalePrice)~log(GrLivArea) + Neighborhood, data = hdT2Updated)
+summary(fit3)
+par(mfrow=c(2,2))
+
+plot(fit3)
+
+#applying predictions to the other dataset
+predictions <- predict(fit, newdata = hd2Updated)
+
+#adding predictions to the data set
+hd2Updated$SalePrice <- predictions
+
+head(hd2Updated)
+
+# Create a scatterplot of Living area v Sale Price - taking log of x value
+hd2Updated %>% ggplot(aes(x = log(GrLivArea), y = SalePrice)) +
+  geom_point()   + geom_smooth(method = "lm", color = "red") +
+  labs(x = "Living area in sqft", y = "Sale Price", title = "Scatterplot of Living area v Sale Price")
+
+
+#checking for any na values in the test set
+sum(is.na(hdT2Updated$SalePrice))
+sum(is.na(hdT2Updated$Id))
+sum(is.na(hdT2Updated$LotArea))
+sum(is.na(hdT2Updated$Neighborhood))
+sum(is.na(hdT2Updated$YearBuilt))
+sum(is.na(hdT2Updated$GrLivArea))
+#checking for na values in the train set
+sum(is.na(hd2Updated$SalePrice))
+sum(is.na(hd2Updated$Id))
+sum(is.na(hd2Updated$LotArea))
+sum(is.na(hd2Updated$Neighborhood))
+sum(is.na(hd2Updated$YearBuilt))
+sum(is.na(hd2Updated$GrLivArea))
+
+
+cor.test(hdT2Updated$SalePrice,hdT2Updated$GrLivArea)
 
 
 
